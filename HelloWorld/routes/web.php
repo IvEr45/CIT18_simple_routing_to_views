f64ea22cf0a9e2ea3aae8fa986561913f6658e9c
IvEr45 <135097522+IvEr45@users.noreply.github.com>
2025-01-16 16:42:42 +0000

    Hello

---
+++ HelloWorld/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//This is the first Route
+Route::get('/greet', function () {
+    return 'Hello, Laravel!';
+//THis is the second Route
+});
+Route::get('/greeting',[GreetController::class, 'greeting']);
